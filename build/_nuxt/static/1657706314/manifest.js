ab4815a0c1e48776e7e7904ce8d069f20c27a885
LinkunGao <gaolinkun123@gmail.com>
2022-08-27 23:57:40 +1200

    test main

--- build/_nuxt/static/1657706314/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002Felectricity-fibrillation","\u002Ffailure-decompensated","\u002Ffailure-healthy","\u002Ffailure-compensated","\u002Fvideo","\u002Fmodel-heart#video-div","\u002Fattack-healthy","\u002Felectricity-healthy","\u002Fabout","\u002Fattack-severe","\u002Fattack-minor","\u002Fmodel-heart"]})
---
+++ docs/source/FAQs/index.rst
@@ -0,0 +1,17 @@
+FAQs
+==========
+
+
+.. toctree::
+    :maxdepth: 2
+    :numbered: 2
+
+    01_question
+    02_question
+    03_question
+    04_question
+    05_question
+    06_question
+    07_question
+    08_question
+    09_question
